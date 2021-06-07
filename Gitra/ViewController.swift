8e9ce8cc737d86c817a997a2e389e3b06bb22773
Christopher Teddy Mienarto <46671469+christoted@users.noreply.github.com>
2021-06-07 12:45:28 +0700

    init

---
+++ Gitra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gitra
+//
+//  Created by Christopher Teddy  on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
