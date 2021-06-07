c438011d96829d8b909ae75f7297f43c413c9d8a
zhafransyahh <zhafransyahh@gmail.com>
2021-06-07 20:16:09 +0700

    Development Resources
    Added Product Sans Fonts, 3 Image and Color assets for development purposes. Added GradientView.

---
+++ Gitra/Resources/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Gitra
+//
+//  Created by Rafi Zhafransyah on 07/06/21.
+//
+
+import UIKit
+
+@IBDesignable class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = UIColor.white
+    @IBInspectable var bottomColor: UIColor = UIColor.black
+
+    override class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+
+    override func layoutSubviews() {
+        (layer as! CAGradientLayer).colors = [topColor.cgColor, bottomColor.cgColor]
+    }
+}
