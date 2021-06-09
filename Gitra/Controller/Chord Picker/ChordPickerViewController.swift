035e0b4d14cf5e83b900da2d4d536b35f1d7e15b
yahyayyash <yahyayyashaa@gmail.com>
2021-06-09 20:48:02 +0700

    Dynamic Picker 40%

--- Gitra/Controller/Chord Picker/ChordPickerViewController.swift
+++ Gitra/Controller/Chord Picker/ChordPickerViewController.swift
@@ -8,15 +8,26 @@
 import UIKit
 
 class ChordPickerViewController: UIViewController {
-
+    
     @IBOutlet weak var chooseButton: UIButton!
     @IBOutlet weak var chordPicker: UIPickerView!
     
     let root = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-    let quality = ["-","Major", "Minor", "Aug", "Dim"]
-    let tension = ["-", "2", "4", "7", "9"]
+    let quality = ["-" : ["-", "7", "7#5", "7b5", "7#9", "7b9", "9", "11", "13"],
+                   "Major" : ["-", "6", "6/9", "7", "9", "11", "13"],
+                   "Minor" : ["-", "6", "7", "7b5", "9", "11", "13"],
+                   "Add" : ["9", "11"],
+                   "Sus" : ["2", "4"],
+                   "Dim" : ["-", "7"],
+                   "Aug" : ["-", "7"]
+    ]
+    //    let quality = ["-","Major", "Minor", "Aug", "Dim"]
+    //    let tension = ["-", "2", "4", "7", "9"]
     
     var chord: [[String]] = []
+    
+    let listOfQuality: [String] = []
+    let listOfTension: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,34 +36,50 @@ class ChordPickerViewController: UIViewController {
         
         chordPicker.dataSource = self
         chordPicker.delegate = self
-                
-        populateChord()
+        
+//        populateChord()
     }
     
-    func populateChord(){
-        chord.append(root)
-        chord.append(quality)
-        chord.append(tension)
-    }
+//    func populateChord(){
+//        chord.append(root)
+//        chord.append(quality)
+//        chord.append(tension)
+//    }
 }
 
 extension ChordPickerViewController: UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return chord.count
+        return 3
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return chord[component].count
+        if component == 0 {
+            return root.count
+        } else if component == 1 {
+            return quality.count
+        }
+        
+        let selectedRow = chordPicker.selectedRow(inComponent: 1)
+        var selectedKey = ""
+        var count = 0
+        
+        for (kind, _ ) in quality {
+            if selectedRow == count {
+                selectedKey = kind
+            }
+            count += 1
+        }
+        return quality[selectedKey]!.count
     }
 }
 
 extension ChordPickerViewController: UIPickerViewDelegate {
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return chord[component][row]
-//    }
+    //    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    //        return chord[component][row]
+    //    }
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-
+        
         var pickerLabel: UILabel? = (view as? UILabel)
         if pickerLabel == nil {
             pickerLabel = UILabel()
@@ -60,22 +87,22 @@ extension ChordPickerViewController: UIPickerViewDelegate {
         }
         pickerLabel?.font = UIFont(name: "Product Sans Bold", size: 42)
         pickerLabel?.text = chord[component][row]
-
+        
         let color = (row == pickerView.selectedRow(inComponent: component)) ? UIColor.orange : UIColor.gray
-
+        
         pickerLabel?.textColor = color
-
+        
         return pickerLabel!
     }
     
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
         return 70
     }
-        
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        if chord[1][chordPicker.selectedRow(inComponent: 1)] == "Major" {
-//            print("test")
-//        }
+        //        if chord[1][chordPicker.selectedRow(inComponent: 1)] == "Major" {
+        //            print("test")
+        //        }
         chordPicker.reloadAllComponents()
     }
 }
