8d741b95b7f651d7ca0a2fbc5c4d609602240db0
Gilang Adrian <gilang.adrian@outlook.com>
2021-06-09 20:25:35 +0700

    Otw setting
    abc

--- Gitra/Controller/Setting/SettingViewController.swift
+++ Gitra/Controller/Setting/SettingViewController.swift
@@ -7,25 +7,61 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+enum typeSetting{
+    case toggle
+    case click
+}
+
+struct settingsType{
+    var titleSettings : String
+    var type : typeSetting
+}
+
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    //var settings = ["Chord input mode", "Welcome screen", "Input command guides", "Chord speed", "Instructions"]
+    let settings = [settingsType(titleSettings: "Chord input mode", type: .click),
+                   settingsType(titleSettings: "Welcome screen", type: .toggle),
+                   settingsType(titleSettings: "Input command guides", type: .toggle),
+                   settingsType(titleSettings: "Chord speed", type: .click),
+                   settingsType(titleSettings: "Instructions", type: .click)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let nib = UINib(nibName: "SettingsTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "SettingsTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // TableView Funcs
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsTableViewCell",
+                                                 for: indexPath) as! SettingsTableViewCell
+        cell.cellLabel.text = settings[indexPath.row].titleSettings
+        cell.cellImageView.backgroundColor = .yellow
+        //cell.textLabel?.text = settings[indexPath.row]
+        
+        return cell
+    }
+    
 }
+
+
+//if type ==.toggle{
+  //  return cellproto1
+//}
+
+//extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+  //  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //    print("You selected \([indexPath.row]).")
+    //}
+//}
