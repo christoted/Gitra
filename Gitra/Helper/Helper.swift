162e1219bff7543840f70dc386ddc32d3910bbba
yahyayyash <yahyayyashaa@gmail.com>
2021-06-14 23:30:23 +0700

    Hampir Final

--- Gitra/Helper/Helper.swift
+++ Gitra/Helper/Helper.swift
@@ -11,73 +11,102 @@ class Helper {
     func convertStringToParam(chord: String) -> String {
         
         let roots = ["C", "D", "E", "F", "G", "A", "B"]
-        let notations = ["flat", "b", "♭", "sharp", "#", "♯"]
-        let qualities = ["major", "minor", "add", "aug", "augmented", "dim", "diminished", "sus", "suspended"]
-        let tensions = ["two", "four", "five", "fifth", "six", "sixth", "seven", "seventh", "nine", "ninth", "eleven", "thirteen",
-                       "2", "4", "5", "6", "7", "9", "11", "13"]
-        
+        let symbols = ["b": ["flat", "b", "♭"],
+                         "#": ["sharp", "#", "♯"]]
+        let qualities = ["maj": ["major", "maj"],
+                         "m": ["minor", "min"],
+                         "add": ["add"],
+                         "aug": ["aug", "augmented"],
+                         "dim": ["dim", "diminished"],
+                         "sus": ["sus", "suspended"]]
+        let tensions = [2: ["2", "two"],
+                        4: ["4", "four"],
+                        5: ["5", "five", "fifth"],
+                        6: ["6", "six", "sixth"],
+                        7: ["7", "seven", "seventh"],
+                        9: ["9", "nine", "ninth"],
+                        11: ["11", "eleven"],
+                        13: ["13", "thirteen"]]
+
+        chord = chord.lowercased()
+
         let splitChordInput = chord.split {
             $0.isWhitespace
         }.map {
             String($0)
         }
-        
-        var chordToParam: String = splitChordInput[0]
-        
-        //contain major without tension
-        for quality in qualities {
-            
+
+        var output = splitChordInput[0]
+
+        //Determine sharp & flat
+        func checkRoots(_ text: String) -> String {
+            for (notation, symbol) in symbols {
+                for char in symbol {
+                    if text.contains(char) {
+                        return notation
+                    }
+                }
+            }
+            return ""
         }
 
-        //contain major with tension
-//        if ((chord.contains("major") || chord.contains("Major")) && (!chord.contains("six") || !chord.contains("seven") || !chord.contains("nine") || !chord.contains("eleven") || !chord.contains("thirteen") )) {
-//            chordToParam.append("")
-//        }
-//
-//        if ((chord.contains("major") || chord.contains("Major")) && (chord.contains("six") || chord.contains("seven") || chord.contains("nine") || chord.contains("eleven") || chord.contains("thirteen") )) {
-//            chordToParam.append("_maj")
-//        }
-//
-//        if (chord.contains("minor")) || chord.contains("Minor"){
-//            chordToParam.append("_m")
-//        }
-//
-//        if (chord.contains("add")) {
-//            chordToParam.append("_add")
-//        }
-//
-//        if (chord.contains("sus")) {
-//            chordToParam.append("_sus")
-//        }
-//
-//        if (chord.contains("dim")) {
-//            chordToParam.append("_dim")
-//        }
-//
-//        if (chord.contains("aug")) {
-//            chordToParam.append("_aug")
-//        }
-//
-//        if ( chord.contains("six") || chord.contains("6")) {
-//            chordToParam.append("6")
-//        }
-//
-//        if ( chord.contains("seven") || chord.contains("7")) {
-//            chordToParam.append("7")
-//        }
-//
-//        if ( chord.contains("nine") || chord.contains("9")) {
-//            chordToParam.append("9")
-//        }
-//
-//        if ( chord.contains("eleven") || chord.contains("11")) {
-//            chordToParam.append("11")
-//        }
-//
-//        if ( chord.contains("thirteen") || chord.contains("13")) {
-//            chordToParam.append("13")
-//        }
+        //Determine chord quality
+        func checkQuality(_ text: String) -> String {
+            for (notation, quality) in qualities {
+                for char in quality {
+                    if text.contains(char) {
+                        return notation
+                    }
+                }
+            }
+            return ""
+        }
+
+        //Determine chord tension
+        func checkTension(_ text: String) -> String {
+            for (number, tension) in tensions {
+                for char in tension {
+                    if text.contains(char) {
+                        return "\(number)"
+                    }
+                }
+            }
+            return ""
+        }
+
+        //Merging input
+        for (index, text) in splitChordInput.enumerated() where index > 0 {
+            if index == 1 && (checkRoots(text) != "") {
+                output.append(checkRoots(text))
+                output.append("_")
+                continue
+            } else if index == 1 && (checkRoots(text) == "") {
+                output.append("_")
+            } else if (index == 1 || index == 2) && checkQuality(text) == "major" {
+                output.append("maj")
+            }
+            
+            output.append(checkQuality(text))
+            output.append(checkRoots(text))
+            output.append(checkTension(text))
+        }
+
+        if output.contains("maj") && !output.contains("maj7") {
+            output.removeLast(4)
+        }
         
-        return chordToParam
+        output.capitalizeFirstLetter()
+        
+        return output
+    }
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+      return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+      self = self.capitalizingFirstLetter()
     }
 }
